f0dacad32cda7c47bd61923b0467891c47067c67
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-04-11 18:19:57 +0200

    house keeping

--- src/test/java/hardwaresimulator/main/MainTest.java
+++ src/test/java/hardwaresimulator/main/MainTest.java
@@ -2,10 +2,17 @@ package hardwaresimulator.main;
 
 import static com.github.stefanbirkner.systemlambda.SystemLambda.assertNothingWrittenToSystemErr;
 import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErr;
+import static hardwaresimulator.main.MainTest.Params.LED_COUNT;
+import static hardwaresimulator.main.MainTest.Params.LED_RINGS;
+import static hardwaresimulator.main.MainTest.Params.LED_SIZE;
+import static hardwaresimulator.main.MainTest.Params.MQTT_HOST;
+import static hardwaresimulator.main.MainTest.Params.MQTT_PORT;
+import static hardwaresimulator.main.MainTest.Params.RING_SIZE;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -14,37 +21,47 @@ import hardwaresimulator.sim.HardwareSimulater.Config;
 
 class MainTest {
 
-	private static final String MQTT_HOST = "-mqttHost";
-	private static final String MQTT_PORT = "-mqttPort";
-	private static final String LED_RINGS = "-rings";
-	private static final String RING_SIZE = "-ringSize";
-	private static final String LED_COUNT = "-ledCount";
-	private static final String LED_SIZE = "-ledSize";
+	public static enum Params {
+		MQTT_HOST("-mqttHost"), //
+		MQTT_PORT("-mqttPort"), //
+		LED_RINGS("-rings"), //
+		RING_SIZE("-ringSize"), //
+		LED_COUNT("-ledCount"), //
+		LED_SIZE("-ledSize"); //
+
+		private final String value;
+
+		private Params(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public static List<String> all() {
+			return Arrays.stream(values()).map(Params::getValue).toList();
+		}
+	}
 
 	@Test
 	void printsHelpForEachOptionOnMinusH() throws Exception {
-		assertThat(tapSystemErr(() -> Main.main("-h"))) //
-				.contains(MQTT_HOST) //
-				.contains(MQTT_PORT) //
-				.contains(LED_RINGS) //
-				.contains(RING_SIZE) //
-				.contains(LED_COUNT) //
-				.contains(LED_SIZE);
+		assertThat(tapSystemErr(() -> Main.main("-h"))).contains(Params.all());
 	}
 
 	@Test
 	void canSetAllOptions() throws Exception {
 		List<String> args = asList( //
-				MQTT_HOST, "a", //
-				MQTT_PORT, "1", //
-				LED_RINGS, "2", //
-				RING_SIZE, "3", //
-				LED_COUNT, "4", //
-				LED_SIZE, "5" //
+				param(MQTT_HOST), "a", //
+				param(MQTT_PORT), "1", //
+				param(LED_RINGS), "2", //
+				param(RING_SIZE), "3", //
+				param(LED_COUNT), "4", //
+				param(LED_SIZE), "5" //
 		);
 
 		assertNothingWrittenToSystemErr(() -> {
-			Config config = execSut(args);
+			Config config = parseArgs(args);
 			assertSoftly(s -> {
 				s.assertThat(config.mqttHost()).isEqualTo("a");
 				s.assertThat(config.mqttPort()).isEqualTo(1);
@@ -57,7 +74,11 @@ class MainTest {
 
 	}
 
-	private Config execSut(List<String> args) {
+	private static String param(Params params) {
+		return params.value;
+	}
+
+	private static Config parseArgs(List<String> args) {
 		return Main.createConfig(args.toArray(String[]::new)).orElse(null);
 	}
 
