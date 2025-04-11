317c9082055ce682375d2b96dd9c6a81ae63746a
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-04-11 18:48:23 +0200

    purged object constructor

--- src/main/java/hardwaresimulator/mqtt/Message.java
+++ src/main/java/hardwaresimulator/mqtt/Message.java
@@ -1,9 +1,4 @@
 package hardwaresimulator.mqtt;
 
 public record Message(String topic, String payload, boolean retained) {
-
-	public Message(String topic, Object payload, boolean retained) {
-		this(topic, payload == null ? null : String.valueOf(payload), retained);
-	}
-
 }
